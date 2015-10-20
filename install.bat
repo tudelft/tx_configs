c169f527cf69fd131940d03e6e43d92e58f893aa
Christophe De Wagter <dewagter@gmail.com>
2015-10-20 17:17:48 +0200

    spash screen copy

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-cp ./devo10_fs/models/*.ini o:/models/ -v 
+cp ./devo10_fs/models/*.ini o:/models/ -v
 cp ./devo10_fs/modelico/*.bmp O:/modelico/ -v
-cp ./devo10_fs/media/*.ini o:/media/ -v 
+cp ./devo10_fs/media/*.bmp o:/media/ -v
